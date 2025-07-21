07800f66dc66c48e9362e241f8f4d5111f4955b8
Tatenda <tatndor@gmail.com>
2025-07-21 08:39:13 +0200

    relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author (using .filter() as required)
+# 1. Query all books by a specific author (uses filter)
 def books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
@@ -18,10 +18,10 @@ def books_in_library(library_name):
     except Library.DoesNotExist:
         return []
 
-# 3. Retrieve the librarian for a library
+# 3. Retrieve the librarian for a library (uses Librarian.objects.get)
 def librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
+        return Librarian.objects.get(library=library)
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
