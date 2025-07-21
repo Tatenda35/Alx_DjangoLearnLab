feecbf5610f1195e94c4dd3d5736cf941f773da1
Tatenda <tatndor@gmail.com>
2025-07-21 10:30:48 +0200

    User Roles

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -30,3 +33,23 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Automatically create or update UserProfile when a User is saved
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Member')  # Default role
+    else:
+        instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library
-
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
 
 # Function-based view
 def list_books(request):
@@ -36,4 +37,23 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
 
+def check_role(role):
+    def inner(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return inner
+
+@login_required
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
